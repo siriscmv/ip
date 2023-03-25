f2fdd6daa789d676e33712f0f91c0f9b8e0420e0
Siris <40269790+Siris01@users.noreply.github.com>
2023-03-25 09:46:30 +0000

    feat: add css in form submission

--- src/ip-lab/WEB-INF/src/FormServlet.java
+++ src/ip-lab/WEB-INF/src/FormServlet.java
@@ -14,37 +14,31 @@ public class FormServlet extends HttpServlet {
         String title = "Form Submission";
 
         out.println( 
-            "<html>\n" +
-            "<head><title>" + title + "</title></head>\n" +
-            "<body bgcolor = \"#f0f0f0\">\n" +
-            "<h1 align = \"center\">" + title + "</h1>\n" +
-            "<table width = \"100%\" border = \"1\" align = \"center\">\n" +
-            "<tr bgcolor = \"#949494\">\n" +
-            "<th>Param Name</th>\n" + 
-            "<th>Param Value(s)</th>\n"+
-            "</tr>\n"
+            "<html>" +
+            "<head>" + 
+            "<title>" + title + "</title>" +
+            "<link rel=\"stylesheet\" href=\"styles/form-table.css\" />" +
+            "</head>" +
+            "<body>" +
+            "<h1>" + title + "</h1>" +
+            "<table>" +
+            "<tr>" +
+            "<th>Param Name</th>" + 
+            "<th>Param Value(s)</th>"+
+            "</tr>"
         );
 
         Enumeration paramNames = request.getParameterNames();
 
         while(paramNames.hasMoreElements()) {
             String paramName = (String)paramNames.nextElement();
-            out.print("<tr><td>" + paramName + "</td>\n<td>");
+            out.print("<tr><td>" + paramName + "</td><td>");
             String[] paramValues = request.getParameterValues(paramName);
 
-            if (paramValues.length == 1) {
-                String paramValue = paramValues[0];
-                if (paramValue.length() == 0) out.println("<i>No Value</i>");
-                else out.println(paramValue);
-            } else {
-                out.println("<ul>");
-                for(int i = 0; i < paramValues.length; i++) {
-                    out.println("<li>" + paramValues[i]);
-                }
-                out.println("</ul>");
-            }
+            if (paramValues[0].length() == 0) out.println("<i>No Value</i>");
+            else out.println(String.join(", ", paramValues));
         }
         
-        out.println("</tr>\n</table>\n</body></html>");
+        out.println("</tr></table></body></html>");
     }
 }
