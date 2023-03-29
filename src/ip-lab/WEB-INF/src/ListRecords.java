7babd349f9d8acd9faba57b1fbba7427b07092ab
Siris <40269790+Siris01@users.noreply.github.com>
2023-03-29 05:03:19 +0000

    feat: list all records page

---
+++ src/ip-lab/WEB-INF/src/ListRecords.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
+
+import java.sql.*;
+
+public class ListRecords extends HttpServlet {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html");
+
+    if (!this.check_auth(request, response)) {
+      return;
+    }
+
+    try {
+
+      Class.forName("com.mysql.jdbc.Driver");
+      Connection con = DriverManager.getConnection("jdbc:mysql://host.docker.internal:3306/test", "test", "test");
+
+      Statement stmt = con.createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT * FROM patient_details");
+      PrintWriter writer = response.getWriter();
+
+      writer.println("<html><head><title>Records</title><link rel=\"stylesheet\" href=\"styles/form-table.css\" /></head>");
+      writer.println("<body><table><tr><th>Id</th><th>Name</th><th>Age</th><th>Gender</th><th>Address</th><th>Martial status</th><th>Date of visit</th>Disease Name</tr>");
+
+      while (rs.next()) {
+        writer.println("<tr>");
+        writer.println("<td>" + rs.getInt("ID") + "</td>");
+        writer.println("<td>" + rs.getString("name") + "</td>");
+        writer.println("<td>" + rs.getInt("age") + "</td>");
+        writer.println("<td>" + rs.getString("gender") + "</td>");
+        writer.println("<td>" + rs.getString("address") + "</td>");
+        writer.println("<td>" + rs.getDate("date_of_visit").toString() + "</td>");
+        writer.println("<td>" + rs.getString("disease_name") + "</td>");
+        writer.println("</tr>");
+      }
+
+      writer.println("</table></body></html>");
+      rs.close();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public boolean check_auth(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    Cookie[] cookies = request.getCookies();
+
+    for (Cookie cookie: cookies) {
+      if (cookie.getName().equals("auth")) {
+        if (cookie.getValue().equals("fed290e7355f56b1bee7717e9b58b92c7f39d98cb709d2322dca50327555a291")) {
+          return true;
+        }
+      }
+    }
+
+    PrintWriter writer = response.getWriter();
+    writer.println("<html><body><h1>Invalid username or password</h1></body></html>");
+
+    return false;
+  }
+}
