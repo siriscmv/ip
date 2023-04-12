69b716d0edb8abbd535bd0ebc34dda05d9d90b61
Siris <40269790+Siris01@users.noreply.github.com>
2023-04-12 15:10:16 +0000

    feat: assignment 7 [sessions]

--- schema.sql
+++ schema.sql
@@ -1,11 +1,17 @@
-CREATE TABLE patient_details (
-    name VARCHAR(255) NOT NULL,
-    age INT NOT NULL,
-    ID INT,
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    dob DATE  NOT NULL,
     gender VARCHAR(1) NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    marital_status BOOLEAN NOT NULL,
-    date_of_visit DATE NOT NULL,
-    disease_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (ID)
+    about VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE sessions (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    token VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
