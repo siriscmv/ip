a597c922ccc9bad1439aea4bc4ed755e4e0554c2
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-04-17 10:25:23 +0530

    feat: countries list

--- schema.sql
+++ schema.sql
@@ -1,17 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE countries (
     id INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    dob DATE  NOT NULL,
-    gender VARCHAR(1) NOT NULL,
-    about VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE sessions (
-    id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    token VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    name VARCHAR(255) NOT NULL,
 );
